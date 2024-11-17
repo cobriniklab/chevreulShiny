4c204b6b6ace4ad822f7891918d78889ba01e0cc
whtns <kevin.stachelek@gmail.com>
2024-11-17 14:38:35 -0800

    trigger rebuild

--- R/utils.R
+++ R/utils.R
@@ -352,3 +352,19 @@ make_chevreul_clean_names <- function(myvec) {
     
     return(myvec)
 }
+
+#' Plotly settings
+#'
+#' Change settings of a plotly plot
+#'
+#' @param plotly_plot  A plotly plot
+#' @param width Default set to '600'
+#' @param height Default set to '700'
+plotly_settings <- function(plotly_plot, width = 600, height = 700) {
+    plotly_plot |>
+        layout(dragmode = "lasso") |>
+        config(toImageButtonOptions = list(format = "svg", 
+                                           filename = "myplot", 
+                                           width = width, height = height)) |>
+        identity()
+}
