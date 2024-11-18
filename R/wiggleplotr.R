a8ebac2807cd7cea1fe748fe0cbfa4820673e5a1
whtns <kevin.stachelek@gmail.com>
2024-11-18 09:58:28 -0800

    trigger rebuild

--- R/wiggleplotr.R
+++ R/wiggleplotr.R
@@ -128,3 +128,37 @@ plot_gene_coverage_by_var <- function(
     
     return(list(plot = coverage_plot, table = base_coverage))
 }
+
+#' Load Bigwigs
+#'
+#' Load a tibble of bigwig file paths by cell id
+#'
+#' @param object A object
+#' @param bigwig_db Sqlite database of bigwig files
+#'
+#' @return a vector of bigwigs file paths
+#' @export
+load_bigwigs <- function(object, bigwig_db = "~/.cache/chevreul/bw-files.db") {
+    con <- dbConnect(SQLite(), dbname = bigwig_db)
+    
+    bigwigfiles <- dbReadTable(con, "bigwigfiles") |>
+        filter(sample_id %in% colnames(object)) |>
+        identity()
+    
+    missing_bigwigs <- colnames(object)[!(colnames(object) %in% 
+                                              bigwigfiles$sample_id)] |>
+        paste(collapse = ", ")
+    
+    if(length(missing_bigwigs)>0){
+        message(paste0("Sample coverage files ", missing_bigwigs, 
+                       "(.bw) do not match samples in object (check file names)"))   
+    }
+    
+    dbDisconnect(con)
+    
+    bigwigfiles <-
+        bigwigfiles |>
+        filter(sample_id %in% colnames(object))
+    
+    return(bigwigfiles)
+}
